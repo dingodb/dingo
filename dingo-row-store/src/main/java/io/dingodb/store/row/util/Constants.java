eaba3c3f97b790d808d28059fd267a3c24496d02
Zongxing Hu <huzongxing0826@gmail.com>
2022-02-08 12:27:39 +0800

    [dingo-row-store] change default region id from -1 to 0

--- dingo-row-store/src/main/java/io/dingodb/store/row/util/Constants.java
+++ dingo-row-store/src/main/java/io/dingodb/store/row/util/Constants.java
@@ -37,7 +37,7 @@ public final class Constants {
     public static final boolean THREAD_AFFINITY_ENABLED
         = SystemPropertyUtil.getBoolean("dingo.thread.affinity.enabled", false);
 
-    public static final String DEFAULT_REGION_ID = "-1L";
+    public static final String DEFAULT_REGION_ID = "0L";
 
     private Constants() {
     }
