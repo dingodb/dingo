068652c153df826f94bd7ec882be6cf1b69edeea
wang.zhen <wangzhenaaa7@gmail.com>
2022-04-07 18:41:21 +0800

    [dingo-exec] fix bug about affected rows when update

---
+++ dingo-test/src/test/resources/table-update-test5-insert.sql
@@ -0,0 +1,4 @@
+insert into strdemo values (1, 'a', 10, 1.0, 'beijing'),(2, 'b', 10, 1.0, 'Being'),(3, 'c',10 , 1.0, 'New York' ),
+                           (4, 'd', 10, 1.0, 'Singapore'),(5, 'e', 10, 1.0,'TaiPei' ),(6, 'f', 10,1.0 , 'Los Angeles'),
+                           (7, 'g',10 , 1.0, 'Hong Kong'),(8, 'h',10 , 1.0, 'London'), (9, 'i',10 ,1.0 , 'SShanghai'),
+                           (10, 'j',10 ,1.0 , 'Tjing' ),(11, 'k',10 ,1.0 , 'Yjing'), (12, 'l',10 ,1.0 , 'Xjing')
