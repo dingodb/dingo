a32bd6cb8ec8d5b99c067781f1ee6c61905e7c65
cuijing <mygalaxycj@126.com>
2022-05-10 21:04:32 +0800

    [dingo-runtime] Fix boolean insert related bugs

---
+++ dingo-test/src/test/resources/table-test-boolean-create.sql
@@ -0,0 +1,8 @@
+create table bldemo(
+id int,
+name varchar(32) not null,
+age int,
+amount double,
+address varchar(255),
+is_delete boolean,
+primary key(id));
---
+++ dingo-test/src/test/resources/table-test-boolean-data.sql
@@ -0,0 +1,4 @@
+insert into bldemo values(1, 'c1', 28, 109.325, 'beijing', true);
+insert into bldemo values(2, 'c2', 17, 139.2, 'beijing', false);
+insert into bldemo values(3, 'c3', 22, 34.89, 'shanghai', 0);
+insert into bldemo values(4, 'c4', 33, 3392.88, 'zhengzhou', 122);
