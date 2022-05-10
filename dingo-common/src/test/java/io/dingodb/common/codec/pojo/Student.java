e35d0a7895eea207e21d196178566528f1d8c325
Zongxing Hu <huzongxing0826@gmail.com>
2022-05-11 07:32:32 +0800

    [dingo-driver] revert convert driver proxy request and response to string to deal null value
    This reverts commit bb2540f6492fbce5d8fb88b707feb2b5e1be5f49.

--- dingo-common/src/test/java/io/dingodb/common/codec/pojo/Student.java
+++ dingo-common/src/test/java/io/dingodb/common/codec/pojo/Student.java
@@ -14,10 +14,19 @@
  * limitations under the License.
  */
 
-package io.dingodb.net.netty.packet;
+package io.dingodb.common.codec.pojo;
 
-public enum PacketMode {
-    USER_DEFINE,
-    GENERIC,
-    API
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class Student {
+    private int id;
+    private String name;
+    private int age;
 }
