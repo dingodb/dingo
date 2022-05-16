80d1c2e43442c086b8f6c335b235ac2dc431b206
cuijing <mygalaxycj@126.com>
2022-05-16 16:20:26 +0800

    [dingo-expr] Fix boolean type related bugs

--- dingo-test/src/test/resources/table-test-boolean-data.sql
+++ dingo-test/src/test/resources/table-test-boolean-data.sql
@@ -2,3 +2,6 @@ insert into bldemo values(1, 'c1', 28, 109.325, 'beijing', true);
 insert into bldemo values(2, 'c2', 17, 139.2, 'beijing', false);
 insert into bldemo values(3, 'c3', 22, 34.89, 'shanghai', 0);
 insert into bldemo values(4, 'c4', 33, 3392.88, 'zhengzhou', 122);
+insert into bldemo values(5, 'c5', 39, 342.01, 'beijing', 2147483648);
+insert into bldemo values(6, 'c6', 11, 3.6, 'zhengzhou', 4294967295);
+insert into bldemo values(7, 'c7', 19, 223.18, 'shanghai', 4294967296);
