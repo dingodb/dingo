99faa7b364ed3d3bfba3fd3ef7fae384884d047d
cuijing <mygalaxycj@126.com>
2022-06-20 17:05:16 +0800

    [dingo-driver] Fix the timestamp precision problem when creating a table

---
+++ dingo-test/src/test/resources/table-test-time-timestamp-create.sql
@@ -0,0 +1,1 @@
+create table t_test(id int,  create_datetime timestamp(3), update_time time(2), primary key (id));
---
+++ dingo-test/src/test/resources/table-test-time-timestamp-data.sql
@@ -0,0 +1,3 @@
+insert into t_test values(1, '2022-06-20 11:49:05.632', '11:50:05.74');
+insert into t_test values(2, '2022-06-20 11:51:23.77', '11:52:23.623');
+insert into t_test values(3, '2022-06-20 11:52:43.8', '11:53:43.8');
