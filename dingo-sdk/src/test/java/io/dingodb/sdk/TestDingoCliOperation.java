1bc72570cbf1c1666937464e8a7ab635cfe73c0b
Zongxing Hu <huzongxing0826@gmail.com>
2022-07-13 18:39:06 +0800

    [dingo-sdk] add Unit Tests about dingoCli operation

---
+++ dingo-sdk/src/test/java/io/dingodb/sdk/TestDingoCliOperation.java
@@ -0,0 +1,345 @@
+/*
+ * Copyright 2021 DataCanvas
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.dingodb.sdk;
+
+import io.dingodb.common.table.TableDefinition;
+import io.dingodb.sdk.annotation.DingoColumn;
+import io.dingodb.sdk.annotation.DingoRecord;
+import io.dingodb.sdk.client.DingoClient;
+import io.dingodb.sdk.client.DingoConnection;
+import io.dingodb.sdk.client.DingoOpCli;
+import io.dingodb.sdk.common.Column;
+import io.dingodb.sdk.common.Key;
+import io.dingodb.sdk.common.Record;
+import io.dingodb.sdk.mock.MockApiRegistry;
+import io.dingodb.sdk.mock.MockMetaClient;
+import io.dingodb.sdk.model.basic.Person;
+import io.dingodb.sdk.model.basic.PersonWithOutKey;
+import io.dingodb.sdk.model.basic.PersonWithoutColumnAnnotation;
+import io.dingodb.sdk.model.basic.PersonWithoutTableName;
+import io.dingodb.sdk.operation.StoreOperationUtils;
+import io.dingodb.sdk.utils.DingoClientException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.lang.reflect.Field;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class TestDingoCliOperation {
+    private Person person = new Person();
+    private DingoClient dingoClient;
+
+    private MockMetaClient metaClient = new MockMetaClient("src/test/resources/config.yaml");
+
+    private MockApiRegistry apiRegistry = new MockApiRegistry();
+
+    @BeforeEach
+    public void init() {
+        person.setFirstName("John");
+        person.setLastName("Doe");
+        person.setSsn("123456789");
+        person.setAge(17);
+        person.setBirthday(new Date());
+        person.setBirthTime(new Time(12, 0, 0));
+        person.setBirthTimestamp(new Timestamp(System.currentTimeMillis()));
+        person.setSalary(1000.0);
+        person.setSalary2(1000.0f);
+
+        dingoClient = new DingoClient("src/test/resources/config/config.yaml");
+        initConnectionInMockMode();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (dingoClient != null) {
+            dingoClient.closeConnection();
+        }
+    }
+
+
+    @Test
+    public void testBasicCreateTable() {
+        boolean isOK = dingoClient.openConnection();
+        Assertions.assertTrue(isOK);
+
+        DingoOpCli dingoCli = new DingoOpCli.Builder(dingoClient).build();
+        isOK = dingoCli.createTable(Person.class);
+        Assertions.assertTrue(isOK);
+        Map<String, TableDefinition> storeOperations = StoreOperationUtils.getTableDefinitionInCache();
+        Assertions.assertEquals(1, storeOperations.size());
+
+        isOK = dingoCli.dropTable(Person.class);
+        Assertions.assertTrue(isOK);
+        storeOperations = StoreOperationUtils.getTableDefinitionInCache();
+        Assertions.assertEquals(0, storeOperations.size());
+    }
+
+    @Test
+    public void testBasicCreateTableCase1() {
+        DingoOpCli dingoCli = new DingoOpCli.Builder(dingoClient).build();
+        boolean isOK = dingoCli.createTable(Person.class);
+        Assertions.assertFalse(isOK);
+        Map<String, TableDefinition> storeOperations = StoreOperationUtils.getTableDefinitionInCache();
+        Assertions.assertEquals(0, storeOperations.size());
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testBasicCreateTableWithoutKey() {
+        boolean isOK = dingoClient.openConnection();
+        Assertions.assertTrue(isOK);
+
+        DingoOpCli dingoCli = new DingoOpCli.Builder(dingoClient).build();
+        try {
+            isOK = dingoCli.createTable(PersonWithOutKey.class);
+        } catch (Exception ex) {
+            Assertions.assertTrue(ex instanceof DingoClientException);
+            Assertions.assertTrue(ex.getMessage().contains("primary key"));
+        }
+        Map<String, TableDefinition> storeOperations = StoreOperationUtils.getTableDefinitionInCache();
+        Assertions.assertEquals(0, storeOperations.size());
+    }
+
+    @Test
+    public void testCreateTableWithOutTableName() {
+        boolean isOK = dingoClient.openConnection();
+        Assertions.assertTrue(isOK);
+
+        DingoOpCli dingoCli = new DingoOpCli.Builder(dingoClient).build();
+        try {
+            isOK = dingoCli.createTable(PersonWithoutTableName.class);
+        } catch (Exception ex) {
+            Assertions.assertTrue(ex instanceof DingoClientException);
+            Assertions.assertTrue(ex.getMessage().contains("TableName not specified"));
+        }
+        Map<String, TableDefinition> storeOperations = StoreOperationUtils.getTableDefinitionInCache();
+        Assertions.assertEquals(0, storeOperations.size());
+    }
+
+    @Test
+    public void testCreateTableWithOutColumnAnnotation() {
+        boolean isOK = dingoClient.openConnection();
+        Assertions.assertTrue(isOK);
+
+        DingoOpCli dingoCli = new DingoOpCli.Builder(dingoClient).build();
+        isOK = dingoCli.createTable(PersonWithoutColumnAnnotation.class);
+        Assertions.assertTrue(isOK);
+        Map<String, TableDefinition> storeOperations = StoreOperationUtils.getTableDefinitionInCache();
+        Assertions.assertEquals(1, storeOperations.size());
+
+        DingoRecord record = PersonWithoutColumnAnnotation.class.getAnnotation(DingoRecord.class);
+        String tableName = record.table();
+        TableDefinition tableDefinition = storeOperations.get(tableName);
+        Assertions.assertTrue(tableDefinition != null);
+
+        int columnCount = tableDefinition.getColumns().size();
+        int loopCnt = 0;
+        for (Field thisField : PersonWithoutColumnAnnotation.class.getDeclaredFields()) {
+            String filedName = thisField.getName();
+            isOK = tableDefinition.getColumns().stream().anyMatch(column -> column.getName().equals(filedName));
+            Assertions.assertTrue(isOK);
+            loopCnt++;
+        }
+        Assertions.assertEquals(columnCount, loopCnt);
+    }
+
+    @Test
+    public void testCreateTableWithColumnAnnotation() {
+        boolean isOK = dingoClient.openConnection();
+        Assertions.assertTrue(isOK);
+
+        DingoOpCli dingoCli = new DingoOpCli.Builder(dingoClient).build();
+        isOK = dingoCli.createTable(Person.class);
+        Assertions.assertTrue(isOK);
+        Map<String, TableDefinition> storeOperations = StoreOperationUtils.getTableDefinitionInCache();
+        Assertions.assertEquals(1, storeOperations.size());
+
+        DingoRecord record = Person.class.getAnnotation(DingoRecord.class);
+        String tableName = record.table();
+        TableDefinition tableDefinition = storeOperations.get(tableName);
+        Assertions.assertTrue(tableDefinition != null);
+
+        int columnCount = tableDefinition.getColumns().size();
+        int loopCnt = 0;
+        for (Field thisField : Person.class.getDeclaredFields()) {
+            String filedName = thisField.getName();
+            if (thisField.isAnnotationPresent(DingoColumn.class)) {
+                DingoColumn thisColumn = thisField.getAnnotation(DingoColumn.class);
+                filedName = thisColumn.name();
+            }
+            String finalColumnName = filedName;
+            isOK = tableDefinition.getColumns().stream().anyMatch(column -> column.getName().equals(finalColumnName));
+            Assertions.assertTrue(isOK);
+            loopCnt++;
+        }
+        Assertions.assertEquals(columnCount, loopCnt);
+    }
+
+    @Test
+    public void testSaveBasicObject() {
+        boolean isOK = dingoClient.openConnection();
+        Assertions.assertTrue(isOK);
+
+        DingoClient spyClient = Mockito.spy(dingoClient);
+        DingoOpCli dingoCli = new DingoOpCli.Builder(spyClient).build();
+        isOK = dingoCli.createTable(Person.class);
+        Assertions.assertTrue(isOK);
+        Map<String, TableDefinition> storeOperations = StoreOperationUtils.getTableDefinitionInCache();
+        Assertions.assertEquals(1, storeOperations.size());
+
+        DingoRecord record = Person.class.getAnnotation(DingoRecord.class);
+        String tableName = record.table();
+        TableDefinition tableDefinition = storeOperations.get(tableName);
+        Assertions.assertTrue(tableDefinition != null);
+        Record expectedRecord = new Record(person, false);
+
+        try {
+            doReturn(true).when(spyClient).put(any(), (Column[]) any());
+            doReturn(expectedRecord).when(spyClient).get((Key) any());
+            doReturn(true).when(spyClient).delete((Key) any());
+        } catch (Exception e) {
+            Assertions.fail("Mock catch Unexpected exception");
+        }
+        dingoCli.save(person);
+
+        Person localPerson = dingoCli.read(Person.class, "123456789");
+        Assertions.assertEquals(person.getSsn(), localPerson.getSsn());
+        Assertions.assertEquals(person.getAge(), localPerson.getAge());
+        Assertions.assertEquals(person.getBirthTimestamp(), localPerson.getBirthTimestamp());
+        System.out.println("Person: " + person);
+        isOK = dingoCli.delete(person);
+        Assertions.assertTrue(isOK);
+    }
+
+    @Test
+    public void testReadMultiRecords() {
+        boolean isOK = dingoClient.openConnection();
+        Assertions.assertTrue(isOK);
+
+        DingoClient spyClient = Mockito.spy(dingoClient);
+        DingoOpCli dingoCli = new DingoOpCli.Builder(spyClient).build();
+        isOK = dingoCli.createTable(Person.class);
+        Assertions.assertTrue(isOK);
+        Map<String, TableDefinition> storeOperations = StoreOperationUtils.getTableDefinitionInCache();
+        Assertions.assertEquals(1, storeOperations.size());
+
+        DingoRecord record = Person.class.getAnnotation(DingoRecord.class);
+        String tableName = record.table();
+        TableDefinition tableDefinition = storeOperations.get(tableName);
+        Assertions.assertTrue(tableDefinition != null);
+        String key1 = "123456789";
+        person.setSsn(key1);
+        Record expectedRecord01 = new Record(person, false);
+        String key2 = "234567891";
+        person.setSsn(key2);
+        Record expectedRecord02 = new Record(person, false);
+
+        try {
+            doReturn(true).when(spyClient).put(any(), (Column[]) any());
+            when(spyClient.get((Key) any()))
+                .thenReturn(expectedRecord01)
+                .thenReturn(expectedRecord02);
+        } catch (Exception e) {
+            Assertions.fail("Mock catch Unexpected exception");
+        }
+        person.setSsn(key1);
+        dingoCli.save(person);
+        person.setSsn(key2);
+        dingoCli.save(person);
+
+        Person localPerson = dingoCli.read(Person.class, key1);
+        Assertions.assertEquals(key1, localPerson.getSsn());
+        Assertions.assertEquals(person.getAge(), localPerson.getAge());
+        Assertions.assertEquals(person.getBirthTimestamp(), localPerson.getBirthTimestamp());
+
+        localPerson = dingoCli.read(Person.class, key2);
+        Assertions.assertEquals(key2, localPerson.getSsn());
+        Assertions.assertEquals(person.getAge(), localPerson.getAge());
+        Assertions.assertEquals(person.getBirthTimestamp(), localPerson.getBirthTimestamp());
+        System.out.println("Person: " + person);
+    }
+
+    @Test
+    public void testPutBatch2Table() throws Exception {
+        boolean isOK = dingoClient.openConnection();
+        Assertions.assertTrue(isOK);
+
+        DingoClient spyClient = Mockito.spy(dingoClient);
+        DingoOpCli dingoCli = new DingoOpCli.Builder(spyClient).build();
+        isOK = dingoCli.createTable(Person.class);
+        Assertions.assertTrue(isOK);
+
+        List<String> userKeys = Arrays.asList("123456789", "234567891", "345678912", "456789123");
+        List<Person> users = new ArrayList<>();
+        for (String key : userKeys) {
+            Person localPerson = new Person(person);
+            localPerson.setSsn(key);
+            users.add(localPerson);
+        }
+        try {
+            doReturn(true).when(spyClient).put(any(), (Column[]) any());
+            // this method is equals to dingoCli.save(users.toArray());
+            // dingoCli.save(users.get(0), users.get(1), users.get(2), users.get(3));
+            dingoCli.save(users.toArray());
+        } catch (Exception e) {
+            Assertions.fail("Mock catch Unexpected exception");
+        }
+    }
+
+
+    private void initConnectionInMockMode() {
+        DingoConnection connection = mock(DingoConnection.class);
+        when(connection.getMetaClient()).thenReturn(metaClient);
+        when(connection.getApiRegistry()).thenReturn(apiRegistry);
+
+        try {
+            Field metaClientField = DingoConnection.class.getDeclaredField("metaClient");
+            metaClientField.setAccessible(true);
+            metaClientField.set(connection, metaClient);
+
+            Field apiRegistryField = DingoConnection.class.getDeclaredField("apiRegistry");
+            apiRegistryField.setAccessible(true);
+            apiRegistryField.set(connection, apiRegistry);
+
+            Field connectionField = DingoClient.class.getDeclaredField("connection");
+            connectionField.setAccessible(true);
+            connectionField.set(dingoClient, connection);
+        } catch (NoSuchFieldException e) {
+            Assertions.fail("DingoConnection.metaClient field not found");
+        } catch (SecurityException e) {
+            Assertions.fail("DingoConnection.metaClient field not accessible");
+        } catch (IllegalAccessException e) {
+            Assertions.fail("Invalid Runtime Exception");
+        }
+    }
+
+
+}
---
+++ dingo-sdk/src/test/java/io/dingodb/sdk/mock/MockMetaServiceAPI.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2021 DataCanvas
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.dingodb.sdk.mock;
+
+import io.dingodb.common.CommonId;
+import io.dingodb.common.Location;
+import io.dingodb.common.table.TableDefinition;
+import io.dingodb.common.util.ByteArrayUtils;
+import io.dingodb.meta.Part;
+import io.dingodb.server.api.MetaServiceApi;
+
+import java.util.List;
+import java.util.Map;
+import java.util.NavigableMap;
+import javax.annotation.Nonnull;
+
+public class MockMetaServiceAPI implements MetaServiceApi {
+
+    @Override
+    public void createTable(@Nonnull String tableName, @Nonnull TableDefinition tableDefinition) {
+        return ;
+    }
+
+    @Override
+    public boolean dropTable(@Nonnull String tableName) {
+        return false;
+    }
+
+    @Override
+    public byte[] getTableKey(@Nonnull String tableName) {
+        return new byte[0];
+    }
+
+    @Override
+    public CommonId getTableId(@Nonnull String tableName) {
+        return null;
+    }
+
+    @Override
+    public byte[] getIndexId(@Nonnull String tableName) {
+        return new byte[0];
+    }
+
+    @Override
+    public Map<String, TableDefinition> getTableDefinitions() {
+        return null;
+    }
+
+    @Override
+    public NavigableMap<ByteArrayUtils.ComparableByteArray, Part> getParts(String name) {
+        return null;
+    }
+
+    @Override
+    public List<Location> getDistributes(String name) {
+        return null;
+    }
+
+    @Override
+    public TableDefinition getTableDefinition(@Nonnull CommonId commonId) {
+        return null;
+    }
+}
