aeac81c3c73d2a5bfcddbe7b60f557d8b5d81c7c
Zongxing Hu <huzongxing0826@gmail.com>
2022-07-18 18:40:03 +0800

    [dingo-sdk] add unit tests about complex struct

---
+++ dingo-sdk/src/test/java/io/dingodb/sdk/TestDingoEmbeddedType.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2021 DataCanvas
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.dingodb.sdk;
+
+import io.dingodb.common.table.TableDefinition;
+import io.dingodb.sdk.annotation.DingoRecord;
+import io.dingodb.sdk.client.DingoClient;
+import io.dingodb.sdk.client.DingoOpCli;
+import io.dingodb.sdk.common.Column;
+import io.dingodb.sdk.common.Key;
+import io.dingodb.sdk.common.Record;
+import io.dingodb.sdk.mock.MockApiRegistry;
+import io.dingodb.sdk.mock.MockMetaClient;
+import io.dingodb.sdk.model.AnnotatedArrayClass;
+import io.dingodb.sdk.model.ChildClass;
+import io.dingodb.sdk.model.ComplexStruct;
+import io.dingodb.sdk.model.embeded.OwnedClass;
+import io.dingodb.sdk.model.embeded.OwningClass;
+import io.dingodb.sdk.operation.StoreOperationUtils;
+import io.dingodb.sdk.utils.MetaServiceUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
+
+
+public class TestDingoEmbeddedType {
+
+    private DingoClient dingoClient;
+
+    private MockMetaClient metaClient = new MockMetaClient("src/test/resources/config.yaml");
+
+    private MockApiRegistry apiRegistry = new MockApiRegistry();
+
+    @BeforeEach
+    public void init() {
+        dingoClient = new DingoClient("src/test/resources/config/config.yaml");
+        MetaServiceUtils.initConnectionInMockMode(dingoClient, metaClient, apiRegistry);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (dingoClient != null) {
+            dingoClient.closeConnection();
+        }
+    }
+
+    @Test
+    public void testDingoEmbededType01() {
+        boolean isOK = dingoClient.openConnection();
+        Assertions.assertTrue(isOK);
+
+        DingoOpCli dingoCli = new DingoOpCli.Builder(dingoClient).build();
+        isOK = dingoCli.createTable(OwningClass.class);
+        Assertions.assertTrue(isOK);
+        Map<String, TableDefinition> storeOperations = StoreOperationUtils.getTableDefinitionInCache();
+        Assertions.assertEquals(1, storeOperations.size());
+
+        isOK = dingoCli.dropTable(OwningClass.class);
+        Assertions.assertTrue(isOK);
+        storeOperations = StoreOperationUtils.getTableDefinitionInCache();
+        Assertions.assertEquals(0, storeOperations.size());
+    }
+
+    @Test
+    public void testEmbededType02() {
+        boolean isOK = dingoClient.openConnection();
+        Assertions.assertTrue(isOK);
+
+        DingoClient spyClient = Mockito.spy(dingoClient);
+        DingoOpCli dingoCli = new DingoOpCli.Builder(spyClient).build();
+        isOK = dingoCli.createTable(OwningClass.class);
+        Assertions.assertTrue(isOK);
+
+        Map<String, TableDefinition> storeOperations = StoreOperationUtils.getTableDefinitionInCache();
+        Assertions.assertEquals(1, storeOperations.size());
+
+        OwningClass owner = new OwningClass();
+        owner.id = 1;
+        owner.children.add(new OwnedClass("a", 1, new Date()));
+        owner.children.add(new OwnedClass("b", 2, new Date(new Date().getTime() - 5000)));
+        owner.children.add(new OwnedClass("c", 3, new Date(new Date().getTime() - 10000)));
+
+        try {
+            doReturn(true).when(spyClient).put(any(), (Column[]) any());
+            doReturn(true).when(spyClient).delete((Key) any());
+            doReturn(true).when(spyClient).dropTable(anyString());
+        } catch (Exception e) {
+            Assertions.fail("Mock catch Unexpected exception");
+        }
+        dingoCli.save(owner);
+
+    }
+
+    @Test
+    public void testEmbedType03() {
+        OwningClass owner = new OwningClass();
+        owner.id = 1;
+        owner.children.add(new OwnedClass("a", 1, new Date()));
+        owner.children.add(new OwnedClass("b", 2, new Date(new Date().getTime() - 5000)));
+        owner.children.add(new OwnedClass("c", 3, new Date(new Date().getTime() - 10000)));
+    }
+}
