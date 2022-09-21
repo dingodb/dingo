89758d977be9d8e7278986fb9375afdd9bdb440d
Zongxing Hu <huzongxing0826@gmail.com>
2022-09-21 19:13:32 +0800

    [dingo-cli] hotfix bugs when import json files

--- dingo-cli/src/main/java/io/dingodb/cli/source/impl/fetch/JsonFetch.java
+++ dingo-cli/src/main/java/io/dingodb/cli/source/impl/fetch/JsonFetch.java
@@ -21,9 +21,14 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.dingodb.cli.source.Fetch;
 import io.dingodb.cli.source.impl.AbstractParser;
+import io.dingodb.common.table.ColumnDefinition;
 import io.dingodb.common.table.TableDefinition;
 import io.dingodb.sdk.client.DingoClient;
 import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -31,8 +36,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
-import java.util.stream.Collectors;
 
 @Slf4j
 public class JsonFetch extends AbstractParser implements Fetch {
@@ -47,43 +52,101 @@ public class JsonFetch extends AbstractParser implements Fetch {
             DingoClient dingoClient,
             TableDefinition tableDefinition) {
         try {
-            List<Object[]> records = new ArrayList<>();
-            BufferedReader br = new BufferedReader(new FileReader(localFile));
             long totalReadCnt = 0L;
             long totalWriteCnt = 0L;
-            String line = "";
-            while ((line = br.readLine()) != null) {
-                line = line.trim();
-                if (!isValidRecord(line)) {
-                    continue;
-                }
-                // skip the header line '[{"id":1,"name":"dingo"},{"id":2,"name":"dingo"}]'
-                if (line.charAt(0) == '[') {
-                    line = line.substring(1);
-                }
-                if (line.charAt(line.length() - 1) == ']') {
-                    line = line.substring(0, line.length() - 1);
-                }
+            JSONParser parser = new JSONParser();
+            JSONArray jsonArray = (JSONArray) parser.parse(new FileReader(localFile));
 
-                records.add(readLine(line).values().toArray());
+            for (Object o : jsonArray) {
+                JSONObject jsonObject = (JSONObject) o;
                 totalReadCnt++;
-                if (records.size() >= 1000) {
-                    totalWriteCnt += this.parse(tableDefinition, records, dingoClient);
-                    records.clear();
-                }
-            }
-            if (records.size() != 0) {
+                List<Object[]> records = new ArrayList<>();
+                records.add(parseSingleRow(jsonObject.toString(), tableDefinition));
                 totalWriteCnt += this.parse(tableDefinition, records, dingoClient);
             }
-            System.out.println("The total read count from File is:" + totalReadCnt
+            System.out.println("FileMode=>The total read count from File is:" + totalReadCnt
                 + ", real write count:" + totalWriteCnt);
-        } catch (IOException e) {
-            log.error("Error reading file:{}", localFile, e);
+            return;
+        } catch (IOException fileException) {
+            log.error("Error file:{} catch IO Exception:{}",
+                localFile, fileException.toString(), fileException);
+        } catch (ParseException parseException) {
+            log.error("Error file:{} catch parser exception:{}",
+                localFile, parseException.toString(), parseException);
+        }
+
+        try {
+            doParseJsonRecord(localFile, dingoClient, tableDefinition);
+        } catch (IOException exception) {
+            log.error("IO Exception:{}", exception.toString(), exception);
         }
     }
 
+    private void doParseJsonRecord(final String localFile,
+                                   final DingoClient dingoClient,
+                                   final TableDefinition tableDefinition) throws IOException {
+        long totalReadCnt = 0L;
+        long totalWriteCnt = 0L;
+        String line = "";
+        List<Object[]> records = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new FileReader(localFile));
+
+        while ((line = br.readLine()) != null) {
+            line = line.trim();
+            if (!isValidRecord(line)) {
+                continue;
+            }
+            // skip the header line '[{"id":1,"name":"dingo"},{"id":2,"name":"dingo"}]'
+            if (line.charAt(0) == '[') {
+                line = line.substring(1);
+            }
+            if (line.charAt(line.length() - 1) == ']') {
+                line = line.substring(0, line.length() - 1);
+            }
+
+            records.add(parseSingleRow(line, tableDefinition));
+            totalReadCnt++;
+            if (records.size() >= 1000) {
+                totalWriteCnt += this.parse(tableDefinition, records, dingoClient);
+                records.clear();
+            }
+        }
+        if (records.size() != 0) {
+            totalWriteCnt += this.parse(tableDefinition, records, dingoClient);
+        }
+        System.out.println("LineMode=>The total read count from File is:" + totalReadCnt
+            + ", real write count:" + totalWriteCnt);
+    }
+
     @Override
-    public void fetch(Properties props, String topic, DingoClient dingoClient, TableDefinition tableDefinition) {
+    public void fetch(Properties props,
+                      String topic,
+                      DingoClient dingoClient,
+                      TableDefinition tableDefinition) {
+    }
+
+    @Override
+    public long parse(TableDefinition tableDefinition, List<Object[]> records, DingoClient dingoClient) {
+        return super.parse(tableDefinition, records, dingoClient);
+    }
+
+    private Object[] parseSingleRow(String line, TableDefinition definition) throws JsonProcessingException {
+        LinkedHashMap<String, Object> resultOfLinkedMap = mapper.readValue(line, new TypeReference<LinkedHashMap<String, Object>>() {});
+
+        int columnSize = resultOfLinkedMap.keySet().size();
+        List<Object> resultList = new ArrayList<>(columnSize);
+
+        /**
+         * to avoid column name is upper case or low case.
+         */
+        for (ColumnDefinition columnDefinition: definition.getColumns()) {
+            for (String columnName: resultOfLinkedMap.keySet()) {
+                if (columnName.compareToIgnoreCase(columnDefinition.getName()) == 0) {
+                    resultList.add(resultOfLinkedMap.get(columnName));
+                }
+            }
+        }
+        return resultList.toArray();
     }
 
     private boolean isValidRecord(final String inputLine) {
@@ -100,14 +163,5 @@ public class JsonFetch extends AbstractParser implements Fetch {
             return false;
         }
         return true;
-    }
-
-    @Override
-    public long parse(TableDefinition tableDefinition, List<Object[]> records, DingoClient dingoClient) {
-        return super.parse(tableDefinition, records, dingoClient);
-    }
-
-    private LinkedHashMap<String, Object> readLine(String line) throws JsonProcessingException {
-        return mapper.readValue(line, new TypeReference<LinkedHashMap<String, Object>>() {});
     }
 }
