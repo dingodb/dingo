645ca6e2a53253f51687b14f10e2fa135fd8638a
Zongxing Hu <huzongxing0826@gmail.com>
2022-12-19 16:44:44 +0800

    [dingo-dist] update start script about dingo tools

--- dingo-dist/bin/tools.sh
+++ dingo-dist/bin/tools.sh
@@ -21,7 +21,7 @@ NET_JAR_PATH=$(find $ROOT -name dingo-net-*.jar)
 
 java ${JAVA_OPTS} \
      -Dlogback.configurationFile=file:${ROOT}/conf/logback.xml \
-     -classpath ${JAR_PATH}${NET_JAR_PATH} \
+     -classpath ${JAR_PATH}:${NET_JAR_PATH} \
      io.dingodb.cli.Tools \
      --config ${ROOT}/conf/client.yaml \
      $@
