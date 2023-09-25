e9f72e46d07957e25d49cdd6f54f56f248af9f84
guojn1 <gjn16637211661@163.com>
2023-09-25 17:49:40 +0800

    [fix][dingo-exec] Modify vector extract function

--- dingo-exec/src/main/java/io/dingodb/exec/restful/RestfulRequest.java
+++ dingo-exec/src/main/java/io/dingodb/exec/restful/RestfulRequest.java
@@ -30,6 +30,6 @@ public class RestfulRequest {
     private String param;
 
     public String getUrl() {
-        return restUrl.replace("host", host) + param;
+        return restUrl.replace("host", host);
     }
 }
--- dingo-exec/src/main/java/io/dingodb/exec/restful/VectorExtract.java
+++ dingo-exec/src/main/java/io/dingodb/exec/restful/VectorExtract.java
@@ -29,9 +29,9 @@ public class VectorExtract {
     private static Map<String, RestfulRequest> vectorInterface = new HashMap<>();
 
     static {
-        vectorInterface.put(VectorImageFun.NAME, new RestfulRequest("", null, "GET", ""));
+        vectorInterface.put(VectorImageFun.NAME, new RestfulRequest("", null, "PUT", ""));
         vectorInterface.put(VectorTextFun.NAME,
-            new RestfulRequest("http://host:8080/embedding/", null, "GET", ""));
+            new RestfulRequest("http://host:8080/text2vec", null, "PUT", ""));
     }
 
     public static Float[] getVector(String funName, String host, Object param) {
@@ -39,15 +39,22 @@ public class VectorExtract {
             host = host.replace("'", "");
         }
         RestfulRequest request = vectorInterface.get(funName.toLowerCase());
-        request.setParam(param.toString());
+        Map<String, String> paramMap = new HashMap<>();
+        paramMap.put("text_str", param.toString());
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            request.setParam(objectMapper.writeValueAsString(paramMap));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
         request.setHost(host);
         String response = connection.getVal(request);
         if (response == null) {
             throw new RuntimeException("vector load error");
         }
-        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectMapper objectMapperRes = new ObjectMapper();
         try {
-            return objectMapper.readValue(response, Float[].class);
+            return objectMapperRes.readValue(response, Float[].class);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
