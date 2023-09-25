e12e5b807226a0a9a161557c84b6cac2503ffe13
Tao Wang <18234030530@163.com>
2023-09-25 18:54:09 +0800

    [dingo-web] Refactor response structure of http interface

--- dingo-web/src/main/java/io/dingodb/web/controller/IndexController.java
+++ dingo-web/src/main/java/io/dingodb/web/controller/IndexController.java
@@ -21,16 +21,15 @@ import io.dingodb.client.common.IndexDefinition;
 import io.dingodb.common.partition.PartitionDefinition;
 import io.dingodb.common.partition.PartitionDetailDefinition;
 import io.dingodb.common.util.Optional;
-import io.dingodb.sdk.common.index.HnswParam;
 import io.dingodb.sdk.common.index.Index;
 import io.dingodb.sdk.common.index.IndexMetrics;
 import io.dingodb.sdk.common.index.IndexParameter;
 import io.dingodb.sdk.common.index.VectorIndexParameter;
+import io.dingodb.web.Result;
 import io.dingodb.web.mapper.EntityMapper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -56,69 +55,97 @@ public class IndexController {
 
     @ApiOperation("Create index")
     @PostMapping("/api/{schema}")
-    public ResponseEntity<Boolean> crateIndex(@PathVariable String schema, @RequestBody IndexDefinition definition) {
-        if (!definition.getIsAutoIncrement()) {
-            definition.setAutoIncrement(0L);
+    public Result<Boolean> crateIndex(@PathVariable String schema, @RequestBody IndexDefinition definition) {
+        try {
+            if (!definition.getIsAutoIncrement()) {
+                definition.setAutoIncrement(0L);
+            }
+            return Result.ok(dingoClient.createIndex(schema, definition.getName(), definition));
+        } catch (Exception e) {
+            return Result.errorMsg(e.getMessage());
         }
-        return ResponseEntity.ok(dingoClient.createIndex(schema, definition.getName(), definition));
     }
 
     @ApiOperation("Update index")
     @PutMapping("/api/{schema}")
-    public ResponseEntity<Boolean> updateIndex(@PathVariable String schema, @RequestBody IndexDefinition definition) {
-        return ResponseEntity.ok(dingoClient.updateIndex(schema, definition.getName(), definition));
+    public Result<Boolean> updateIndex(@PathVariable String schema, @RequestBody IndexDefinition definition) {
+        try {
+            return Result.ok(dingoClient.updateIndex(schema, definition.getName(), definition));
+        } catch (Exception e) {
+            return Result.errorMsg(e.getMessage());
+        }
     }
 
     @ApiOperation("Update hnsw max_elements")
     @PutMapping("/api/{schema}/{index}/{maxElements}")
-    public ResponseEntity<Boolean> updateIndex(@PathVariable String schema, @PathVariable String index, @PathVariable Integer maxElements) {
-        Index oldIndex = dingoClient.getIndex(schema, index);
-        VectorIndexParameter vectorIndexParameter = oldIndex.getIndexParameter().getVectorIndexParameter();
-        if (vectorIndexParameter.getHnswParam() == null) {
-            return ResponseEntity.ok(false);
-        }
-        vectorIndexParameter.getHnswParam().setMaxElements(maxElements);
-        IndexDefinition newIndex = IndexDefinition.builder()
-            .name(index)
-            .replica(oldIndex.getReplica())
-            .version(oldIndex.getVersion())
-            .isAutoIncrement(oldIndex.getIsAutoIncrement())
-            .autoIncrement(oldIndex.getAutoIncrement())
-            .indexPartition(Optional.mapOrGet(oldIndex.getIndexPartition(), __ -> new PartitionDefinition(
-                oldIndex.getIndexPartition().getFuncName(),
-                oldIndex.getIndexPartition().getCols(),
-                oldIndex.getIndexPartition().getDetails().stream()
-                    .map(d -> new PartitionDetailDefinition(d.getPartName(), d.getOperator(), d.getOperand()))
-                    .collect(Collectors.toList())), () -> null))
-            .indexParameter(new IndexParameter(
-                oldIndex.getIndexParameter().getIndexType(),
-                new VectorIndexParameter(vectorIndexParameter.getVectorIndexType(), vectorIndexParameter.getHnswParam())))
-            .build();
+    public Result<Boolean> updateIndex(@PathVariable String schema, @PathVariable String index, @PathVariable Integer maxElements) {
+        try {
+            Index oldIndex = dingoClient.getIndex(schema, index);
+            VectorIndexParameter vectorIndexParameter = oldIndex.getIndexParameter().getVectorIndexParameter();
+            if (vectorIndexParameter.getHnswParam() == null) {
+                return Result.errorMsg("Only max_elements in hnsw can be modified");
+            }
+            vectorIndexParameter.getHnswParam().setMaxElements(maxElements);
+            IndexDefinition newIndex = IndexDefinition.builder()
+                .name(index)
+                .replica(oldIndex.getReplica())
+                .version(oldIndex.getVersion())
+                .isAutoIncrement(oldIndex.getIsAutoIncrement())
+                .autoIncrement(oldIndex.getAutoIncrement())
+                .indexPartition(Optional.mapOrGet(oldIndex.getIndexPartition(), __ -> new PartitionDefinition(
+                    oldIndex.getIndexPartition().getFuncName(),
+                    oldIndex.getIndexPartition().getCols(),
+                    oldIndex.getIndexPartition().getDetails().stream()
+                        .map(d -> new PartitionDetailDefinition(d.getPartName(), d.getOperator(), d.getOperand()))
+                        .collect(Collectors.toList())), () -> null))
+                .indexParameter(new IndexParameter(
+                    oldIndex.getIndexParameter().getIndexType(),
+                    new VectorIndexParameter(vectorIndexParameter.getVectorIndexType(), vectorIndexParameter.getHnswParam())))
+                .build();
 
-        return ResponseEntity.ok(dingoClient.updateIndex(schema, index, newIndex));
+            return Result.ok(dingoClient.updateIndex(schema, index, newIndex));
+        } catch (Exception e) {
+            return Result.errorMsg(e.getMessage());
+        }
     }
 
     @ApiOperation("Drop index")
     @DeleteMapping("/api/{schema}/{index}")
-    public ResponseEntity<Boolean> deleteIndex(@PathVariable String schema, @PathVariable String index) {
-        return ResponseEntity.ok(dingoClient.dropIndex(schema, index));
+    public Result<Boolean> deleteIndex(@PathVariable String schema, @PathVariable String index) {
+        try {
+            return Result.ok(dingoClient.dropIndex(schema, index));
+        } catch (Exception e) {
+            return Result.errorMsg(e.getMessage());
+        }
     }
 
     @ApiOperation("Get index")
     @GetMapping("/api/{schema}/{index}")
-    public ResponseEntity<Index> get(@PathVariable String schema, @PathVariable String index) {
-        return ResponseEntity.ok(dingoClient.getIndex(schema, index));
+    public Result<Index> get(@PathVariable String schema, @PathVariable String index) {
+        try {
+            return Result.ok(dingoClient.getIndex(schema, index));
+        } catch (Exception e) {
+            return Result.errorMsg(e.getMessage());
+        }
     }
 
     @ApiOperation("Get index metrics")
     @GetMapping("/api/{schema}/{index}/metrics")
-    public ResponseEntity<IndexMetrics> getIndexMetrics(@PathVariable String schema, @PathVariable String index) {
-        return ResponseEntity.ok(dingoClient.getIndexMetrics(schema, index));
+    public Result<IndexMetrics> getIndexMetrics(@PathVariable String schema, @PathVariable String index) {
+        try {
+            return Result.ok(dingoClient.getIndexMetrics(schema, index));
+        } catch (Exception e) {
+            return Result.errorMsg(e.getMessage());
+        }
     }
 
     @ApiOperation("Get index names")
     @GetMapping("/api/{schema}")
-    public ResponseEntity<List<String>> getNames(@PathVariable String schema) {
-        return ResponseEntity.ok(dingoClient.getIndexes(schema).stream().map(Index::getName).collect(Collectors.toList()));
+    public Result<List<String>> getNames(@PathVariable String schema) {
+        try {
+            return Result.ok(dingoClient.getIndexes(schema).stream().map(Index::getName).collect(Collectors.toList()));
+        } catch (Exception e) {
+            return Result.errorMsg(e.getMessage());
+        }
     }
 }
