7585caf55ebd0d084c32786d750658c36e8d59ed
guojn1 <gjn16637211661@163.com>
2025-02-19 14:08:04 +0800

    [feature][dingo-calcite] Support window function

---
+++ dingo-calcite/src/main/java/io/dingodb/calcite/rule/dingo/DingoWindowRule.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2021 DataCanvas
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.dingodb.calcite.rule.dingo;
+
+import io.dingodb.calcite.rel.DingoWindow;
+import io.dingodb.calcite.rel.logical.LogicalScanWithRelOp;
+import io.dingodb.calcite.traits.DingoConvention;
+import org.apache.calcite.plan.Convention;
+import org.apache.calcite.plan.RelTraitSet;
+import org.apache.calcite.rel.RelNode;
+import org.apache.calcite.rel.convert.ConverterRule;
+import org.apache.calcite.rel.core.Window;
+import org.apache.calcite.rel.logical.LogicalWindow;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+public class DingoWindowRule extends ConverterRule {
+    public static final Config DEFAULT = Config.INSTANCE
+        .withConversion(
+            LogicalWindow.class,
+            Convention.NONE,
+            DingoConvention.INSTANCE,
+            "DingoWindowRule"
+        )
+        .withRuleFactory(DingoWindowRule::new);
+
+    protected DingoWindowRule(Config config) {
+        super(config);
+    }
+
+    @Override
+    public @Nullable RelNode convert(RelNode rel) {
+        final Window winAgg = (Window) rel;
+        final RelTraitSet traitSet =
+            winAgg.getTraitSet().replace(DingoConvention.INSTANCE);
+        final RelNode child = winAgg.getInput();
+        final RelNode convertedChild =
+            convert(child,
+                child.getTraitSet().replace(DingoConvention.INSTANCE));
+        return new DingoWindow(rel.getCluster(), traitSet, winAgg.getHints(), convertedChild,
+            winAgg.getConstants(), winAgg.getRowType(), winAgg.groups);
+    }
+}
