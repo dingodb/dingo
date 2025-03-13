265986bb21a850071fb773ae05e645159d82c4ee
gjn <gjn16637211661@163.com>
2025-03-13 11:30:42 +0800

    [fix][dingo-calcite] If binary is incompatible with other types, set the resultType to binary (#1368)
    * [fix][dingo-calcite] If binary is incompatible with other types, set the resultType to binary
    
    * [fix][dingo-executor] Support multyStatement

--- dingo-calcite/src/main/java/io/dingodb/calcite/rule/DingoGetByIndexRule.java
+++ dingo-calcite/src/main/java/io/dingodb/calcite/rule/DingoGetByIndexRule.java
@@ -195,8 +195,9 @@ public class DingoGetByIndexRule extends ConverterRule {
         // get all index definition
         // find match first index to use;
         // new DingoGetIndex
-        boolean disableIndex = !scan.getHints().isEmpty()
-            && "disable_index".equalsIgnoreCase(scan.getHints().get(0).hintName);
+        //boolean disableIndex = !scan.getHints().isEmpty()
+        //    && "disable_index".equalsIgnoreCase(scan.getHints().get(0).hintName);
+        boolean disableIndex = 1 == 1;
         if (disableIndex) {
             return null;
         }
