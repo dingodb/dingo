e5c79122762fee12b95e64efdcfc648b499a01f2
guojn1 <gjn16637211661@163.com>
2025-04-17 14:15:37 +0800

    [fix][dingo-exec] Query partition failed when there is a view available

--- dingo-dist/bin/import-allschema.sh
+++ dingo-dist/bin/import-allschema.sh
@@ -8,6 +8,10 @@ export meta=meta
 
 for i in `cat $srcPath/database.list`
 do
+          if [ $i == 'information_schema' ] ; then
+                echo "skip schema:$i"
+                continue
+          fi
           echo "import $i start"
             bash import-schema.sh $i
           echo "import $i end"
