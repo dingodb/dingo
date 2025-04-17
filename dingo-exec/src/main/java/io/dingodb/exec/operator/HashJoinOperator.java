c79522ecabdfcd0c820513f12ae762611064d164
guoxu <13910754971@163.com>
2025-04-17 17:07:38 +0800

    [fix][executor] Make action varchar joins char being compatible with mysql.

--- dingo-exec/src/main/java/io/dingodb/exec/operator/HashJoinOperator.java
+++ dingo-exec/src/main/java/io/dingodb/exec/operator/HashJoinOperator.java
@@ -59,7 +59,7 @@ public class HashJoinOperator extends SoleOutOperator {
             param.setContext(context);
             if (pin == 0) { // left
                 waitRightFinFlag(param);
-                TupleKey leftKey = new TupleKey(leftMapping.revMap(tuple));
+                TupleKey leftKey = HashJoinParam.rtrimTupleKey(new TupleKey(leftMapping.revMap(tuple)));
                 boolean isEmpty = isEmpty(leftKey, param);
                 if (isEmpty && ("inner".equalsIgnoreCase(param.getJoinType()) || "right".equalsIgnoreCase(param.getJoinType()))) {
                     return true;
@@ -85,7 +85,7 @@ public class HashJoinOperator extends SoleOutOperator {
                     return pushToNext(param, edge, context, newTuple);
                 }
             } else if (pin == 1) { //right
-                TupleKey rightKey = new TupleKey(rightMapping.revMap(tuple));
+                TupleKey rightKey = HashJoinParam.rtrimTupleKey(new TupleKey(rightMapping.revMap(tuple)));
                 if (isEmpty(rightKey, param) && "inner".equalsIgnoreCase(param.getJoinType())) {
                     return true;
                 }
@@ -164,8 +164,9 @@ public class HashJoinOperator extends SoleOutOperator {
     }
 
     private static boolean pushToNext(HashJoinParam param, Edge edge, Context context, Object[] tuple) {
+        Object[] tmpTuple = param.rtrimTuple(tuple);
         if (param.getOtherExpr() != null) {
-            Object object = param.getOtherExpr().eval(tuple);
+            Object object = param.getOtherExpr().eval(tmpTuple);
             if (object != null && (Boolean) object) {
                 return edge.transformToNext(context, tuple);
             } else {
