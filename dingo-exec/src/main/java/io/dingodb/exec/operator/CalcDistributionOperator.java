e1f9a007f02fc94ee6b6c7e4b2cd7a4fa5d2d907
Tao Wang <18234030530@163.com>
2025-07-17 18:00:23 +0800

    [fix][dingo-exec] Calc distribution op add retry

--- dingo-exec/src/main/java/io/dingodb/exec/operator/CalcDistributionOperator.java
+++ dingo-exec/src/main/java/io/dingodb/exec/operator/CalcDistributionOperator.java
@@ -16,12 +16,18 @@
 
 package io.dingodb.exec.operator;
 
+import io.dingodb.common.CommonId;
+import io.dingodb.common.config.DingoConfiguration;
+import io.dingodb.common.log.LogUtils;
 import io.dingodb.common.partition.RangeDistribution;
 import io.dingodb.common.util.ByteArrayUtils;
+import io.dingodb.common.util.Optional;
 import io.dingodb.common.util.RangeUtils;
 import io.dingodb.exec.dag.Vertex;
 import io.dingodb.exec.operator.params.DistributionSourceParam;
+import io.dingodb.meta.MetaService;
 import io.dingodb.partition.PartitionService;
+import io.dingodb.store.api.transaction.exception.RegionSplitException;
 import lombok.extern.slf4j.Slf4j;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
@@ -31,6 +37,8 @@ import java.util.NavigableMap;
 import java.util.NavigableSet;
 import java.util.TreeSet;
 
+import static io.dingodb.common.util.Utils.calculatePrefixCount;
+
 @Slf4j
 public class CalcDistributionOperator extends IteratorSourceOperator {
     public static final CalcDistributionOperator INSTANCE = new CalcDistributionOperator();
@@ -38,28 +46,69 @@ public class CalcDistributionOperator extends IteratorSourceOperator {
     private CalcDistributionOperator() {
     }
 
-    @Override
-    protected @NonNull Iterator<Object[]> createIterator(Vertex vertex) {
-        DistributionSourceParam param = vertex.getParam();
+    private static NavigableSet<RangeDistribution> getRangeDistributions(DistributionSourceParam param) {
         PartitionService ps = param.getPs();
+        NavigableMap<ByteArrayUtils.ComparableByteArray, RangeDistribution> rangeDistribution
+            = param.getRangeDistribution();
         byte[] startKey = param.getStartKey();
         byte[] endKey = param.getEndKey();
         boolean withStart = param.isWithStart();
         boolean withEnd = param.isWithEnd();
-        NavigableMap<ByteArrayUtils.ComparableByteArray, RangeDistribution> ranges = param.getRangeDistribution();
         NavigableSet<RangeDistribution> distributions;
         if (param.getFilter() != null || param.isNotBetween()) {
             if (param.isLogicalNot() || param.isNotBetween()) {
                 distributions = new TreeSet<>(RangeUtils.rangeComparator(1));
-                distributions.addAll(ps.calcPartitionRange(null, startKey, true, !withStart, ranges));
-                distributions.addAll(ps.calcPartitionRange(endKey, null, !withEnd, true, ranges));
-            } else {
-                distributions = ps.calcPartitionRange(startKey, endKey, withStart, withEnd, ranges);
+                distributions.addAll(ps.calcPartitionRange(
+                    null,
+                    startKey,
+                    true,
+                    !withStart,
+                    rangeDistribution));
+                distributions.addAll(ps.calcPartitionRange(
+                    endKey,
+                    null,
+                    !withEnd,
+                    true,
+                    rangeDistribution
+                ));
+                return distributions;
             }
-        } else {
-            distributions = ps.calcPartitionRange(startKey, endKey, withStart, withEnd, ranges);
         }
+        return ps.calcPartitionRange(startKey, endKey, withStart, withEnd, rangeDistribution);
+    }
 
+    @Override
+    protected @NonNull Iterator<Object[]> createIterator(Vertex vertex) {
+        DistributionSourceParam param = vertex.getParam();
+        PartitionService ps = param.getPs();
+        NavigableSet<RangeDistribution> distributions = getRangeDistributions(param);
+        Integer retry = Optional.mapOrGet(DingoConfiguration.instance().find("retry", int.class), __ -> __, () -> 30);
+        while (retry-- > 0) {
+            try {
+                if (param.getKeyTuple() == null || distributions.size() > 1) {
+                    return distributions.stream()
+                        .map(d -> new Object[]{d, param.getKeyTuple()})
+                        .iterator();
+                } else {
+                    Object[] keyTuple = param.getKeyTuple();
+                    CommonId partId = ps.calcPartId(
+                        param.getCodec().encodeKeyPrefix(keyTuple, calculatePrefixCount(keyTuple)),
+                        param.getRangeDistribution()
+                    );
+                    RangeDistribution distribution = RangeDistribution.builder().id(partId).build();
+                    return new ArrayList<>(distributions).stream()
+                        .filter(d -> d.getId().equals(distribution.getId()))
+                        .map(d -> new Object[]{d, param.getKeyTuple()})
+                        .iterator();
+                }
+            } catch (RegionSplitException e) {
+                LogUtils.error(log, e.getMessage());
+                NavigableMap<ByteArrayUtils.ComparableByteArray, RangeDistribution> distribution =
+                    MetaService.root().getRangeDistribution(param.getTd().getTableId());
+                param.setRangeDistribution(distribution);
+                distributions = getRangeDistributions(param);
+            }
+        }
         return new ArrayList<>(distributions).stream().map(d -> new Object[]{d, param.getKeyTuple()}).iterator();
     }
 }
