9e8a822c462f4258e536c83a59e9a7bdb64f7697
Dongwei <dongwei@zetyun.com>
2025-07-29 21:22:13 +0800

    [feat] Start a new version v4.0.0

--- dingo-dist/bin/start-executor.sh
+++ dingo-dist/bin/start-executor.sh
@@ -55,7 +55,7 @@ fi
 nohup ${JAVA_HOME}/bin/java ${JAVA_OPTS} \
      --add-opens java.base/java.util=ALL-UNNAMED \
      --add-opens java.base/java.lang=ALL-UNNAMED \
-     -Dlogback.configurationFile=file:${ROOT}/conf/logback-executor-release.xml \
+     -Dlogback.configurationFile=file:${ROOT}/conf/logback-executor.xml \
      -classpath ${JAR_PATH}:${NET_JAR_PATH}:${LOCAL_STORE_JAR_PATH}  \
      io.dingodb.server.executor.Starter \
      --config ${ROOT}/conf/executor.yaml \
