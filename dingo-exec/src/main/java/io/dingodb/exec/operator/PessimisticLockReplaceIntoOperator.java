bb6513b69ba455f98bab7b570b105dff87e875a7
guoxu <13910754971@163.com>
2025-09-01 16:35:07 +0800

    [fix][dingo-exec] Range checking for tinyint in insert and update statement.

--- dingo-exec/src/main/java/io/dingodb/exec/operator/PessimisticLockReplaceIntoOperator.java
+++ dingo-exec/src/main/java/io/dingodb/exec/operator/PessimisticLockReplaceIntoOperator.java
@@ -97,6 +97,12 @@ public class PessimisticLockReplaceIntoOperator extends SoleOutOperator {
                                 throw new DingoTypeRangeException(0, "Out of range value for column '" + originColumns.get(i).getName() + "'");
                             }
                         }
+                    } else if(originColumns.get(i).getSqlTypeName().equalsIgnoreCase("TINYINT")) {
+                        if(tuple[i] instanceof Integer) {
+                            if ( (Integer)tuple[i] >= 127 || (Integer)tuple[i] <= -128) {
+                                throw new DingoTypeRangeException(0, "Out of range value for column '" + originColumns.get(i).getName() + "'");
+                            }
+                        }
                     }
                 }
             }
--- dingo-exec/src/main/java/io/dingodb/exec/operator/TxnPartUpdateOperator.java
+++ dingo-exec/src/main/java/io/dingodb/exec/operator/TxnPartUpdateOperator.java
@@ -131,6 +131,12 @@ public class TxnPartUpdateOperator extends PartModifyOperator {
                                 throw new DingoTypeRangeException(0, "Out of range value for column '" + originColumns.get(index).getName() + "'");
                             }
                         }
+                    } else if(originColumns.get(index).getSqlTypeName().equalsIgnoreCase("TINYINT")) {
+                        if(newValue instanceof Integer) {
+                            if ( (Integer)newValue >= 127 || (Integer)newValue <= -128) {
+                                throw new DingoTypeRangeException(0, "Out of range value for column '" + originColumns.get(index).getName() + "'");
+                            }
+                        }
                     }
                 }
 
