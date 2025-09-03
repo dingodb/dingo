e5b9fe732f525e1a0f040e36276446cb24efbf64
Tao Wang <18234030530@163.com>
2025-09-03 18:37:47 +0800

    [fix][dingo-exec] Correct retry when regionSplit occurs

--- dingo-exec/src/main/java/io/dingodb/exec/operator/NewCalcDistributionOperator.java
+++ dingo-exec/src/main/java/io/dingodb/exec/operator/NewCalcDistributionOperator.java
@@ -16,22 +16,17 @@
 
 package io.dingodb.exec.operator;
 
-import com.codahale.metrics.Gauge;
 import com.google.common.base.Supplier;
-import io.dingodb.common.CommonId;
 import io.dingodb.common.concurrent.Executors;
-import io.dingodb.common.config.DingoConfiguration;
 import io.dingodb.common.log.LogUtils;
 import io.dingodb.common.partition.RangeDistribution;
 import io.dingodb.common.util.ByteArrayUtils;
-import io.dingodb.common.util.Optional;
 import io.dingodb.common.util.RangeUtils;
 import io.dingodb.common.util.Utils;
 import io.dingodb.exec.dag.Edge;
 import io.dingodb.exec.dag.Vertex;
 import io.dingodb.exec.operator.data.Context;
 import io.dingodb.exec.operator.params.DistributionSourceParam;
-import io.dingodb.meta.MetaService;
 import io.dingodb.partition.PartitionService;
 import io.dingodb.store.api.transaction.exception.LockWaitException;
 import io.dingodb.store.api.transaction.exception.RegionSplitException;
@@ -40,7 +35,6 @@ import org.checkerframework.checker.nullness.qual.NonNull;
 
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.NavigableMap;
 import java.util.NavigableSet;
 import java.util.Set;
@@ -112,25 +106,10 @@ public class NewCalcDistributionOperator extends SourceOperator {
             }
         }
         boolean parallel = Utils.parallel(param.getKeepOrder());
-        Integer retry = Optional.mapOrGet(DingoConfiguration.instance()
-            .find("retry", int.class), __ -> __, () -> 120);
         boolean flag = (!parallel || distributions.size() == 1);
-        boolean isSplit = false;
-        while (retry-- > 0 && flag) {
-            try {
-                push(context, vertex, distributions, param);
-                break;
-            } catch (RegionSplitException e) {
-                LogUtils.error(log, e.getMessage());
-                NavigableMap<ByteArrayUtils.ComparableByteArray, RangeDistribution> newDistribution =
-                    MetaService.root().getRangeDistribution(param.getTd().getTableId());
-                param.setRangeDistribution(newDistribution);
-                distributions = getRangeDistributions(param);
-                isSplit = true;
-                flag = distributions.size() == 1;
-            }
-        }
-        if (!flag || (isSplit && distributions.size() > 1)) {
+        if (flag) {
+            push(context, vertex, distributions, param);
+        } else {
             try {
                 int concurrencyLevel = param.getConcurrencyLevel();
                 Set<CompletableFuture<Boolean>> futures = new HashSet<>(concurrencyLevel);
@@ -151,6 +130,9 @@ public class NewCalcDistributionOperator extends SourceOperator {
                 if (exception.getCause() instanceof LockWaitException) {
                     throw new LockWaitException("Lock wait");
                 }
+                if (exception.getCause() instanceof RegionSplitException) {
+                    throw new RegionSplitException(exception.getCause().getMessage());
+                }
                 throw exception;
             }
         }
@@ -160,51 +142,15 @@ public class NewCalcDistributionOperator extends SourceOperator {
     private static void push(Context context,
                              @NonNull Vertex vertex,
                              Set<RangeDistribution> distributions, DistributionSourceParam param) {
-        Integer maxRetry = Optional.mapOrGet(DingoConfiguration.instance()
-            .find("retry", int.class), __ -> __, () -> 120);
         for (RangeDistribution distribution : distributions) {
-            try {
-                if (log.isTraceEnabled()) {
-                    LogUtils.trace(log, "Push distribution: {}", distribution);
+            if (log.isTraceEnabled()) {
+                LogUtils.trace(log, "Push distribution: {}", distribution);
+            }
+            context.setDistribution(distribution);
+            for (Edge edge : vertex.getOutList()) {
+                if (!edge.transformToNext(context, param.getKeyTuple())) {
+                    break;
                 }
-                context.setDistribution(distribution);
-                for (Edge edge : vertex.getOutList()) {
-                    if (!edge.transformToNext(context, param.getKeyTuple())) {
-                        break;
-                    }
-                }
-            } catch (Exception ex) {
-                if (ex instanceof RegionSplitException
-                    || (ex.getMessage() != null && ex.getMessage().contains("epoch is not match, region_epoch"))) {
-                    int retry;
-                    if (param.getSplitRetry().containsKey(distribution.getId())) {
-                        int retryCnt = param.getSplitRetry().get(distribution.getId());
-                        retry = retryCnt + 1;
-                    } else {
-                        retry = 1;
-                    }
-                    if (retry > 10) {
-                        MetaService.root().invalidateDistribution(param.getTd().getTableId());
-                    }
-                    if (retry > maxRetry) {
-                        LogUtils.error(log, ex.getMessage(), ex);
-                        throw new RuntimeException("The number of split retries exceeds the maximum limit");
-                    }
-                    param.getSplitRetry().put(distribution.getId(), retry);
-
-                    NavigableMap<ByteArrayUtils.ComparableByteArray, RangeDistribution> tmpDistribution =
-                        MetaService.root().getRangeDistribution(param.getTd().tableId);
-                    DistributionSourceParam copyParam = param.copy(
-                        tmpDistribution,
-                        distribution.getStartKey(),
-                        distribution.getEndKey(),
-                        distribution.isWithStart(),
-                        distribution.isWithEnd());
-                    NavigableSet<RangeDistribution> rangeDistributions = getRangeDistributions(copyParam);
-                    push(context, vertex, rangeDistributions, param);
-                    return;
-                }
-                throw ex;
             }
         }
     }
@@ -224,48 +170,13 @@ public class NewCalcDistributionOperator extends SourceOperator {
             return vertex.getOutList().stream()
                 .map(out -> out.transformToNext(copyContext, param.getKeyTuple())).toList().get(0);
         };
-        Integer maxRetry = Optional.mapOrGet(DingoConfiguration.instance()
-            .find("retry", int.class), __ -> __, () -> 120);
         return CompletableFuture.supplyAsync(
             supplier, Executors.executor(
                 "operator-" + vertex.getTask().getJobId() + "-"
                     + vertex.getTask().getId() + "-" + vertex.getId() + "-" + distribution.getId()))
             .exceptionally(ex -> {
                 if (ex != null) {
-                    if (ex.getCause() instanceof RegionSplitException
-                        || (ex.getMessage() != null && ex.getMessage().contains("epoch is not match, region_epoch"))) {
-                        int retry;
-                        if (param.getSplitRetry().containsKey(distribution.getId())) {
-                            int retryCnt = param.getSplitRetry().get(distribution.getId());
-                            retry = retryCnt + 1;
-                        } else {
-                            retry = 1;
-                        }
-                        if (retry > 10) {
-                            MetaService.root().invalidateDistribution(param.getTd().getTableId());
-                        }
-                        if (retry > maxRetry) {
-                            LogUtils.error(log, ex.getMessage(), ex);
-                            throw new RuntimeException("The number of split retries exceeds the maximum limit");
-                        }
-                        param.getSplitRetry().put(distribution.getId(), retry);
-                        NavigableMap<ByteArrayUtils.ComparableByteArray, RangeDistribution> tmpDistribution =
-                            MetaService.root().getRangeDistribution(param.getTd().getTableId());
-                        DistributionSourceParam copyParam = param.copy(
-                            tmpDistribution,
-                            distribution.getStartKey(),
-                            distribution.getEndKey(),
-                            distribution.isWithStart(),
-                            distribution.isWithEnd());
-                        NavigableSet<RangeDistribution> rangeDistributions = getRangeDistributions(copyParam);
-                        Set<CompletableFuture<Boolean>> futures = new HashSet<>(param.getConcurrencyLevel());
-                        for (RangeDistribution rangeDistribution : rangeDistributions) {
-                            futures.add(push(context, vertex, param, rangeDistribution));
-                        }
-                        if (!futures.isEmpty()) {
-                            CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
-                        }
-                    } else if (ex.getCause() instanceof LockWaitException) {
+                    if (ex.getCause() instanceof LockWaitException) {
                         LogUtils.error(log, "jobId:" + vertex.getTask().getJobId() + ", taskId:"
                             + vertex.getTask().getId() + ", vertexId:" + vertex.getId() + ", error:", ex);
                         throw new LockWaitException("Lock wait");
