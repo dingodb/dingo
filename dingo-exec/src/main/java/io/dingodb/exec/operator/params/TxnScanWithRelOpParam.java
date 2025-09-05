08d9a893fffe34a0d658f08eacef91c8f3ef39ce
guoxu <13910754971@163.com>
2025-09-05 17:46:54 +0800

    [fix][dingo-calcite] Fix issue about boolean/timestamp union all.Fix issue column position selection when group aggregation.

--- dingo-exec/src/main/java/io/dingodb/exec/operator/params/TxnScanWithRelOpParam.java
+++ dingo-exec/src/main/java/io/dingodb/exec/operator/params/TxnScanWithRelOpParam.java
@@ -117,7 +117,7 @@ public class TxnScanWithRelOpParam extends ScanWithRelOpParam {
                 SelectionObj selectionObj = new SelectionObj(selections, true);
                 boolean isSelection = false;
                 if (isAutoCommit() && RelOpSelectionVisitor.INSTANCE.visit(relOp, selectionObj) == SelectionFlag.OK
-                    && selectionObj.isProject() && selections.size() != selection.size()) {
+                    && selectionObj.isProject() && selections.size() != selection.size() && !selections.isEmpty()) {
                     try {
                         selection.clear();
                         selection.addAll(selections);
