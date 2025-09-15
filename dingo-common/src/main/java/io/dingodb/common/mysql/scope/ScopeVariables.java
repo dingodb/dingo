0d33efb9f5933f5b00b25b59264a7c5e8c2464fa
guojn1 <gjn16637211661@163.com>
2025-09-15 20:28:35 +0800

    [fix][dingo-driver] Add variables to controll alias case sensitivity

--- dingo-common/src/main/java/io/dingodb/common/mysql/scope/ScopeVariables.java
+++ dingo-common/src/main/java/io/dingodb/common/mysql/scope/ScopeVariables.java
@@ -224,6 +224,15 @@ public final class ScopeVariables {
         }
     }
 
+    public static boolean aliasCaseSensitivity() {
+        try {
+            String aliasCaseSensitivity = executorProp.getOrDefault("alias_case_sensitivity", "false").toString();
+            return "true".equalsIgnoreCase(aliasCaseSensitivity);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
     public static synchronized void setExecutorProp(String key, String val) {
         if ("rpc_batch_size".equalsIgnoreCase(key)) {
             int rpcBatchSize = Integer.parseInt(val);
