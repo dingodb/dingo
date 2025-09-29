ca888359548fd137ca1e434a4efeda897e727657
guoxu <13910754971@163.com>
2025-09-29 09:41:47 +0800

    [fix][dingo-calcite] Fix issue that composite index can not be updated.

--- dingo-calcite/src/main/java/io/dingodb/calcite/visitor/function/DingoStreamingConverterVisitFun.java
+++ dingo-calcite/src/main/java/io/dingodb/calcite/visitor/function/DingoStreamingConverterVisitFun.java
@@ -133,9 +133,12 @@ public final class DingoStreamingConverterVisitFun {
         }
         if (dstPartitions.size() < media.getPartitions().size()) {
             assert dstDistribution == null && dstPartitions.isEmpty() || dstPartitions.size() == 1;
-            if (dstDistribution instanceof DingoRelPartitionByIndex && ((DingoRelPartitionByIndex) dstDistribution).getTargetTableList().size() > 1) {
+
+            if (dstDistribution instanceof DingoRelPartitionByIndex &&
+                ((DingoRelPartitionByIndex) dstDistribution).getTargetTableList().stream().distinct().toList().size() > 1) {
                 return outputs;
             }
+
             outputs = DingoCoalesce.coalesce(idGenerator, outputs, dstPartitions, media.getPartitions());
         }
         return outputs;
