da08dbc9c46117e320d33b30abf0d6e50230e1e7
Tao Wang <18234030530@163.com>
2025-11-06 17:17:24 +0800

    [fix][dingo-calcite] Add executor shuffle global variable, default off

--- dingo-calcite/src/main/java/io/dingodb/calcite/visitor/function/DingoStreamingConverterVisitFun.java
+++ dingo-calcite/src/main/java/io/dingodb/calcite/visitor/function/DingoStreamingConverterVisitFun.java
@@ -259,7 +259,8 @@ public final class DingoStreamingConverterVisitFun {
         List<Vertex> outputs = new LinkedList<>();
         List<Location> locations = new ArrayList<>();
         // todo transaction is AutoCommit join cross node
-        if (transaction == null || transaction.isAutoCommit() || visitor.getExecuteVariables().isJoinConcurrency()) {
+        if (visitor.getExecuteVariables().isExecutorShuffle() && (
+            transaction == null || transaction.isAutoCommit() || visitor.getExecuteVariables().isJoinConcurrency())) {
             locations.addAll(ClusterService.getDefault().getComputingLocations());
         }
         final HashStrategy hs = new SimpleHashStrategy();
