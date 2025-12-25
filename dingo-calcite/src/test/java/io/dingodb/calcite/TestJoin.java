3da486249bd3a4b449fb1e644ce0a8027c7ca9f2
Tao Wang <18234030530@163.com>
2025-12-25 17:09:18 +0800

    [fix][dingo-calcite] Fix join test

--- dingo-calcite/src/test/java/io/dingodb/calcite/TestJoin.java
+++ dingo-calcite/src/test/java/io/dingodb/calcite/TestJoin.java
@@ -97,6 +97,7 @@ public class TestJoin {
         RelNode optimized = parser.optimize(relRoot.rel);
         AssertRelNode assertJoin = Assert.relNode(optimized)
             .isA(DingoRoot.class).streaming(DingoRelStreaming.ROOT)
+            .soleInput().isA(DingoStreamingConverter.class)
             .soleInput().isA(DingoHashJoin.class).prop("joinType", JoinRelType.INNER).inputNum(2);
         assertJoin.input(0).isA(DingoStreamingConverter.class)
             .soleInput().isA(DingoScanWithRelOp.class);
